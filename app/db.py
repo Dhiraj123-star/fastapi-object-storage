2f9a0e09b56c65a3e922fa3ffbd57a9fee04fe22
Dhiraj Kumar <gdhiraj499@gmail.com>
2026-03-25 11:54:44 +0530

    feat: add database connection setup

---
+++ app/db.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker,declarative_base
+
+DATABASE_URL= "postgresql://admin:password@postgres:5432/filesdb"
+
+engine= create_engine(DATABASE_URL)
+
+SessionLocal =sessionmaker(bind=engine)
+
+Base=declarative_base()
