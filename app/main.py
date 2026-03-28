f19dc58843ef59d8a5fa74af146934e2ad566b59
Dhiraj Kumar <gdhiraj499@gmail.com>
2026-03-29 01:20:24 +0530

    feat: add JWT auth with HTTPBearer for swagger support

--- app/main.py
+++ app/main.py
@@ -1,140 +1,130 @@
-from fastapi import FastAPI, UploadFile,File,Depends
+from fastapi import FastAPI, UploadFile,File,Depends,HTTPException
+from fastapi.security import HTTPBearer,HTTPAuthorizationCredentials
+from sqlalchemy.orm import Session
 import uuid
 import os
 
-from db import SessionLocal, engine
-from models import Base, FileMetadata
-from storage import s3, BUCKET,create_bucket
+from db import Base,engine,get_db
+from models import Base, FileMetadata,User
+from storage import upload_file,list_files,delete_file,generate_download_url,create_bucket
+from auth import hash_password,verify_password,create_access_token,verify_token
 
 app =FastAPI(
     title= "FastAPI Object Storage API",
-    description= "S3-compatible object storage service using MinIO with PostgreSQL metadata management.",
+    description= "S3-compatible object storage service using MinIO with JWT auth and PostgreSQL metadata management.",
     version="1.0.0"
 )
 
 # Create tables
 Base.metadata.create_all(bind=engine)
+create_bucket()
 
-# DB dependency
-def get_db():
-    db= SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+security=HTTPBearer()
+
+# ---------------- AUTH Headers ---------- # 
+def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    token = credentials.credentials
+    payload = verify_token(token)
+    return payload["sub"]
+
+# -----------  AUTH APIs -------------------- #
+@app.post("/register")
+def register(username:str,password:str,db:Session=Depends(get_db)):
+    user = User(
+        id= str(uuid.uuid4()),
+        username = username,
+        password = hash_password(password)
+    )
+    db.add(user)
+    db.commit()
+    return {"message":"User created"}
 
 
-@app.on_event("startup")
-def startup():
-    buckets=s3.list_buckets()["Buckets"]
-    names = [b["Name"]for b in buckets]
+@app.post("/login")
+def login(username:str,password:str,db:Session=Depends(get_db)):
+    user = db.query(User).filter(User.username==username).first()
 
-    if BUCKET not in names:
-        s3.create_bucket(Bucket=BUCKET)
+    if not user or not verify_password(password,user.password):
+        raise HTTPException(status_code=401,detail="Invalid credentials")
 
-# Upload with optional folder support
+    token = create_access_token({"sub":user.username})
+    return {"access_token":token}
+
+
+
+
+# ---------------- FILE APIs ---------------- #
+
 @app.post("/upload")
-async def upload(file: UploadFile=File(...),user:str="default",db=Depends(get_db)):
+def upload(
+    file: UploadFile = File(...),
+    current_user: str = Depends(get_current_user),
+    db: Session = Depends(get_db)
+):
+    file_id = str(uuid.uuid4())
+    key = f"{current_user}/{file_id}_{file.filename}"
 
-    # extract extension
-    ext = os.path.splitext(file.filename)[1]
-    file_id=str(uuid.uuid4())
-    # generate uuid filename
-    unique_name = f"{file_id}{ext}"
+    upload_file(file.file, key)
 
-    key = f"{user}/{unique_name}"
-
-    # upload to s3
-    s3.upload_fileobj(
-        file.file,
-        BUCKET,
-        key
-    )
-    # Save metadata in DB
-    file_meta = FileMetadata(
-        id= file_id,
-        user=user,
-        original_name = file.filename,
-        s3_key = key,
+    metadata = FileMetadata(
+        id=file_id,
+        user=current_user,
+        original_name=file.filename,
+        s3_key=key
     )
 
-    db.add(file_meta)
+    db.add(metadata)
     db.commit()
 
     return {
-        "id": file_id,
-        "stored_as":key,
-        "original_name": file.filename
+        "file_id": file_id,
+        "stored_as": key
     }
 
-# Presigned Upload URL
-@app.get("/upload-url")
-def get_upload_url(filename:str,folder:str=""):
-    key= f"{folder}/{filename}" if folder else filename
 
-    url = s3.generate_presigned_url(
-        ClientMethod="put_object",
-        Params={"Bucket":BUCKET,"Key":key},
-        ExpiresIn=3000,
-    )
-    return {"upload_url":url,"key":key}
-
-# List files from DB
 @app.get("/files")
-def list_files(db=Depends(get_db)):
-    files = db.query(FileMetadata).all()
-    
-    return [
-        {
-            "id":f.id,
-            "user":f.user,
-            "original_name":f.s3_key,
-            "uploaded_at":f.uploaded_at
-        }
-        for f in files
-    ]
-    
+def get_files(current_user: str = Depends(get_current_user)):
+    files = list_files()
+    user_files = [f for f in files if f.startswith(current_user)]
+    return {"files": user_files}
 
-# Download 
-@app.get("/download/{file_id}")
-def download(file_id: str,db=Depends(get_db)):
-
-    file = db.query(FileMetadata).filter(FileMetadata.id==file_id).first()
-
-    if not file:
-        return {"error":"File not found"}
-    url = s3.generate_presigned_url (
-        "get_object",
-        Params= {"Bucket":BUCKET,"Key":file.s3_key},
-        ExpiresIn=3000,
-    )
-    return {"url":url}
-
-# Delete 
-@app.delete("/delete/{file_id}")
-def delete_file(file_id:str,db=Depends(get_db)):
-    file = db.query(FileMetadata).filter(FileMetadata.id==file_id).first()
-
-    if not file:
-        return {"error":"File not found"}
-
-    s3.delete_object(Bucket=BUCKET,Key=file.s3_key)
-
-    db.delete(file)
-    db.commit()
-    return {"message":"deleted"}
 
 @app.get("/file/{file_id}")
-def get_file(file_id:str,db=Depends(get_db)):
-    file = db.query(FileMetadata).filter(FileMetadata.id==file_id).first()
+def get_file(file_id: str, db: Session = Depends(get_db), current_user: str = Depends(get_current_user)):
+    file = db.query(FileMetadata).filter(FileMetadata.id == file_id).first()
 
-    if not file:
-        return {"error":"File not found"}
-    
+    if not file or file.user != current_user:
+        raise HTTPException(status_code=404, detail="File not found")
+
     return {
-        "id":file.id,
+        "id": file.id,
         "user": file.user,
-        "original_name":file.original_name,
-        "s3_key":file.s3_key,
-        "uploaded_at":file.uploaded_at,
+        "original_name": file.original_name,
+        "s3_key": file.s3_key,
+        "uploaded_at": file.uploaded_at,
     }
+
+
+@app.get("/download/{file_id}")
+def download(file_id: str, db: Session = Depends(get_db), current_user: str = Depends(get_current_user)):
+    file = db.query(FileMetadata).filter(FileMetadata.id == file_id).first()
+
+    if not file or file.user != current_user:
+        raise HTTPException(status_code=404, detail="File not found")
+
+    url = generate_download_url(file.s3_key)
+    return {"download_url": url}
+
+
+@app.delete("/delete/{file_id}")
+def delete(file_id: str, db: Session = Depends(get_db), current_user: str = Depends(get_current_user)):
+    file = db.query(FileMetadata).filter(FileMetadata.id == file_id).first()
+
+    if not file or file.user != current_user:
+        raise HTTPException(status_code=404, detail="File not found")
+
+    delete_file(file.s3_key)
+    db.delete(file)
+    db.commit()
+
+    return {"message": "File deleted"}
